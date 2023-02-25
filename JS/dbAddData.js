e1b4b6124391b2ce860a06a667c5577fa8ef61fe
DarioHaener <69902881+DarioHaener@users.noreply.github.com>
2023-02-25 02:33:38 +0100

    Game commit

--- JS/dbAddData.js
+++ JS/dbAddData.js
@@ -1,28 +1,11 @@
 import db from "JS/dbInitialize.js"
 
-export default async function Write(name, password, geldbetrag, rang, zeitpunkt, spielrunden) {
-    const querySnapshot = await db.collection("User Data")
-        .where("Name", "==", name)
-        .where("Password", "==", password)
-        .limit(1)
-        .get();
-
-    if (!querySnapshot.empty) {
-        const doc = querySnapshot.docs[0];
-        db.collection("User Data").doc(doc.id).update({
-            Geldbetrag: geldbetrag,
-            Rang: rang,
-            Zeitpunkt: zeitpunkt,
-            Spielrunden: spielrunden,
-        });
-    } else {
-        db.collection("User Data").add({
-            Name: name,
-            Password: password,
-            Geldbetrag: geldbetrag,
-            Rang: rang,
-            Zeitpunkt: zeitpunkt,
-            Spielrunden: spielrunden,
-        });
-    }
+export default async function Write(name, geldbetrag, zeitpunkt, spielrunden) {
+    db.collection('User data').add({
+        Name: name,
+        Geldbetrag: geldbetrag,
+        Rang: rang,
+        Zeitpunkt: zeitpunkt,
+        Spielrunden: spielrunden,
+    })
 }
